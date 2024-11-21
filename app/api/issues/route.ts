e29f002ab15cf61bb58ec584979ab711ae58c2da
oleg <olegsukharev1991@gmail.com>
2024-11-21 16:38:19 +0100

    adding bd

---
+++ app/api/issues/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { z } from "zod";
+
+export const issueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required.").max(65535),
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
+    if (!validation.success)
+      return NextResponse.json(validation.error.errors, { status: 400 });
+
+    const newIssue = await prisma.issue.create({
+      data: {
+        title: body.title || "Default Title",
+        description: body.description || "Default Description",
+      },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
+  } catch (error) {
+    console.error("Error creating issue:", error); // Log any error
+    return NextResponse.json({ error: "Failed to create issue" }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const issues = await prisma.issue.findMany();
+    return NextResponse.json(issues, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching issues:", error);
+    return NextResponse.json({ error: "Failed to fetch issues" }, { status: 500 });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,66 @@
+"use client";
+
+// import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Issue {
+  id: string;
+  title: string;
+  description: string;
+}
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Homepage</main>
-  );
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchIssues = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/issues", { signal });
+        const data = await res.json();
+
+        console.log(data, 'data');
+
+        setIssues(data);
+      } catch (error) {
+        console.error("Error fetching issues:", error);
+      }
+    };
+
+    fetchIssues();
+
+    return () => controller.abort();
+  }, [])
+
+  const createIssue = () => {
+    fetch("/api/issues", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "1",
+        description: "2",
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error: ${res.statusText}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIssues((prevIssues) => [...prevIssues, data]);
+      })
+      .catch((error) => {
+        console.error("Something went wrong:", error.message);
+      });
+  };
+
+  return <div>
+    <pre>{JSON.stringify(issues, null, 2)}</pre>
+    <button onClick={createIssue}>Click me</button>
+  </div>;
 }
