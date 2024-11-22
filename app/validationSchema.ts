23cedb32daa0d587f3021ba3ddc4fbb1872a9a06
oleg <olegsukharev1991@gmail.com>
2024-11-23 00:26:42 +0100

    createIssueSchema,updating the error message for the discription field if it is empty

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,9 @@ import { z } from "zod";
 
 export const createIssueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
-  description: z.string().min(1, "Description is required.").max(65535),
+  description: z.string({ required_error: 'Description is required.' })
+    .min(1, 'Description is required.').max(65535),
 });
+
+
+    
