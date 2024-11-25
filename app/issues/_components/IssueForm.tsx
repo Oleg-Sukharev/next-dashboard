794072cdff80354407b920b9b4e5abe8726365fc
oleg <olegsukharev1991@gmail.com>
2024-11-25 01:37:25 +0100

    Build: API for deleting issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -32,7 +32,12 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      await axios.post('/api/issues', data);
+      if (issue) {
+        await axios.patch('/api/issues/' + issue.id, data);
+      } else {
+        await axios.post('/api/issues', data);
+      }
+
       router.push('/issues');
     } catch (error) {
       setError('An unexpected error occurred.');
@@ -70,7 +75,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <Button disabled={isSubmitting}>
+          {issue ? "Update Issue" : "Submit New Issue"}
+          {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
